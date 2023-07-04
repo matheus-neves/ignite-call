b189114b1111c9ef56c2245df2e0bc9609d2b753
Matheus Neves <matheusneves1989@gmail.com>
2023-07-04 20:39:15 -0300

    feat(utils): implement fetchWrapper

---
+++ src/utils/fetchWrapper.ts
@@ -0,0 +1,25 @@
+import { RequestInit } from 'next/dist/server/web/spec-extension/request'
+
+type RequestInfo = string | Request
+
+interface FetchWrapperParams {
+  url: RequestInfo | URL
+  options?: RequestInit | undefined
+}
+
+export async function fetchWrapper<T = unknown>({
+  url,
+  options,
+}: FetchWrapperParams) {
+  const data = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}${url}`, {
+    headers: {
+      'Content-Type': 'application/json',
+      ...options?.headers,
+    },
+    ...options,
+  })
+
+  const result = await data.json()
+
+  return result as T
+}
