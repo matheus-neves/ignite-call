13d71e0f78846a29698dde0f24295a26f5453660
Matheus Neves <matheusneves1989@gmail.com>
2023-07-06 21:27:40 -0300

    feat(api/users): check if user exists, save cookie

--- src/app/api/users/route.ts
+++ src/app/api/users/route.ts
@@ -1,16 +1,37 @@
 import { prisma } from '@/lib/prisma'
 import { NextResponse } from 'next/server'
+import { cookies } from 'next/headers'
 
 export async function POST(request: Request) {
   const data = await request.json()
 
   const { name, username } = data
 
+  const userExists = await prisma.user.findUnique({
+    where: {
+      username,
+    },
+  })
+
+  if (userExists) {
+    return NextResponse.json(data, {
+      status: 400,
+      statusText: 'Username already taken.',
+    })
+  }
+
   const user = await prisma.user.create({
     data: {
       name,
       username,
     },
+  })
+
+  cookies().set({
+    name: '@ignitecall:userId',
+    value: user.id,
+    maxAge: 60 * 60 * 24 * 7, // 7 days
+    path: '/',
   })
 
   return NextResponse.json(user, { status: 201 })
