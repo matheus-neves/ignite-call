d01a2ad25d3a9127f1247ef66a68f051975f11ff
Matheus Neves <matheusneves1989@gmail.com>
2023-07-08 20:59:19 -0300

    refactor(styles): isolate common styles

--- src/app/register/styles.ts
+++ src/app/register/styles.ts
@@ -1,23 +1,4 @@
-import { styled, Box, Heading, Text } from '@ignite-ui/react'
-
-export const Container = styled('main', {
-  maxWidth: 572,
-  margin: '$20 auto $4',
-  padding: '0 $4',
-})
-
-export const Header = styled('div', {
-  padding: '0 $6',
-
-  [`> ${Heading}`]: {
-    lineHeight: '$base',
-  },
-
-  [`> ${Text}`]: {
-    color: '$gray200',
-    marginBottom: '$6',
-  },
-})
+import { styled, Box, Text } from '@ignite-ui/react'
 
 export const Form = styled(Box, {
   marginTop: '$6',
