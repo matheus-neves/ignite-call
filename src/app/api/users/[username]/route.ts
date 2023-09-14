c1220e0fe5f29f2c39cd2588285075852596d54f
matheus neves <matheusneves1989@gmail.com>
2023-09-14 20:32:42 -0300

    feat(api/users/username): get possibleTimes and availableTimes

---
+++ src/app/api/users/[username]/route.ts
@@ -0,0 +1,87 @@
+/* eslint-disable camelcase */
+import { prisma } from '@/lib/prisma'
+import { NextResponse } from 'next/server'
+import dayjs from 'dayjs'
+
+export async function GET(
+  request: Request,
+  context: { params: { username: string } },
+) {
+  const url = new URL(request.url)
+
+  const date = url.searchParams.get('date')
+
+  const username = context.params.username
+
+  if (!date) {
+    return NextResponse.json({
+      status: 400,
+      statusText: 'Date no provided.',
+    })
+  }
+
+  const user = await prisma.user.findUnique({
+    where: {
+      username,
+    },
+  })
+
+  if (!user) {
+    return NextResponse.json({
+      status: 400,
+      statusText: 'User does not exist.',
+    })
+  }
+
+  const referenceDate = dayjs(String(date))
+  const isPastDate = referenceDate.endOf('day').isBefore(new Date())
+
+  if (isPastDate) {
+    return NextResponse.json({ availability: [] })
+  }
+
+  const userAvailability = await prisma.userTimeInterval.findFirst({
+    where: {
+      user_id: user.id,
+      week_day: referenceDate.get('day'),
+    },
+  })
+
+  if (!userAvailability) {
+    return NextResponse.json({ availability: [] })
+  }
+
+  const { time_start_in_minutes, time_end_in_minutes } = userAvailability
+
+  console.log('userAvailability', userAvailability)
+
+  const startHour = time_start_in_minutes / 60
+  const endHour = time_end_in_minutes / 60
+
+  const possibleTimes = Array.from({ length: endHour - startHour }).map(
+    (_, i) => {
+      return startHour + i
+    },
+  )
+
+  const blockedTimes = await prisma.scheduling.findMany({
+    select: {
+      date: true,
+    },
+    where: {
+      user_id: user.id,
+      date: {
+        gte: referenceDate.set('hour', startHour).toDate(),
+        lte: referenceDate.set('hour', endHour).toDate(),
+      },
+    },
+  })
+
+  const availableTimes = possibleTimes.filter((time) => {
+    return !blockedTimes.some(
+      (blockedTime) => blockedTime.date.getHours() === time,
+    )
+  })
+
+  return NextResponse.json({ possibleTimes, availableTimes })
+}
