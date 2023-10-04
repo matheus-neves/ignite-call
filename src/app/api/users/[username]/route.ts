87567d69ad5f58b1b88577a2d414ba415b2d2345
matheus neves <matheusneves1989@gmail.com>
2023-10-04 19:52:13 -0300

    feat(api/users/username): block past times

--- src/app/api/users/[username]/route.ts
+++ src/app/api/users/[username]/route.ts
@@ -76,9 +76,14 @@ export async function GET(
   })
 
   const availableTimes = possibleTimes.filter((time) => {
-    return !blockedTimes.some(
+
+    const isTimeBlocked = !blockedTimes.some(
       (blockedTime) => blockedTime.date.getHours() === time,
     )
+
+    const isTimeInPast = referenceDate.set('hour', time).isBefore(new Date())
+
+    return !isTimeBlocked && !isTimeInPast
   })
 
   return NextResponse.json({ possibleTimes, availableTimes })
